9f69f17f2f9421d6541f2831edcdb4d1112b9e45
justplius <justplius@gmail.com>
2013-09-21 20:29:34 +0300

    Added another print line

--- delete_procect/src/klase/trinti/lt/klase.java
+++ delete_procect/src/klase/trinti/lt/klase.java
@@ -2,6 +2,7 @@ package klase.trinti.lt;
 
 public class klase {
 	public static void main(String[] args) {
-	    System.out.println("Git is fun");
+		System.out.println("Git is fun");
+		System.out.println("True story");
 	}
 }
